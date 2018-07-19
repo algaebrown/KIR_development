6158dee8e5a7d20c6ec728dfea527096036d2ded
Rachelmarty20 <Rachelmarty20@gmail.com>
2018-07-19 15:13:05 -0700

    Added output to sort

--- data_gathering/bin/convert_to_fastq2.py
+++ data_gathering/bin/convert_to_fastq2.py
@@ -15,9 +15,8 @@ def main(input_bam, sorted_bam, dedup_bam, output_fastq1, system):
     print "Bam is sorted."
 
     # dedup bam file
-
-    print "Bam is deduped."
     bam_remove_dup(sorted_bam, dedup_bam, system)
+    print "Bam is deduped."
 
     # bam to fastq
     bam_to_fastq(dedup_bam, output_fastq1, system)
@@ -27,27 +26,17 @@ def main(input_bam, sorted_bam, dedup_bam, output_fastq1, system):
 ###########################################  Helper Methods  #####################################
 
 
-def bam_to_fastq(f_in, f_out1, system):
-    if system == 'cellar':
-        # Import tools (this will obviously have to change)
-        bedtools="/cellar/users/hcarter/programs/bedtools-2.17.0/bin"
-        cmd = '{0}/bamToFastq -i {1} -fq {2}'.format(bedtools, f_in, f_out1)
-        os.system(cmd)
-    else:
-        cmd = 'bamToFastq -i {0} -fq {1} -fq2 {2}'.format(f_in, f_out1)
-        os.system(cmd)
-
-
 def bam_sort(f_in, f_out, system):
     if system == 'cellar':
         # Import tools (this will obviously have to change)
         samtools="/cellar/users/hcarter/programs/samtools-1.6/samtools"
-        cmd = '{0} sort -n {1} {2}'.format(samtools, f_in, f_out)
+        cmd = '{0} sort -n {1} > {2}'.format(samtools, f_in, f_out)
         os.system(cmd)
     else:
         # TODO: need to check this to run it on licr
         cmd = 'samtools sort -n {0} {1}'.format(f_in, f_out)
         os.system(cmd)
+
 
 def bam_remove_dup(f_in, f_out, system):
     if system == 'cellar':
@@ -61,11 +50,23 @@ def bam_remove_dup(f_in, f_out, system):
         os.system(cmd)
 
 
+def bam_to_fastq(f_in, f_out1, system):
+    if system == 'cellar':
+        # Import tools (this will obviously have to change)
+        bedtools="/cellar/users/hcarter/programs/bedtools-2.17.0/bin"
+        cmd = '{0}/bamToFastq -i {1} -fq {2}'.format(bedtools, f_in, f_out1)
+        os.system(cmd)
+    else:
+        cmd = 'bamToFastq -i {0} -fq {1} -fq2 {2}'.format(f_in, f_out1)
+        os.system(cmd)
+
+
+
+
 ###########################################  Import Arguments  #####################################
 
 if __name__ == "__main__":
     start_time = time.time()
-    print sys.argv
     if len(sys.argv) != 6:
         print "Invalid arguments."
         sys.exit()
