897263d9ee3865f19f8b3914043ca7a17939246b
Rachelmarty20 <Rachelmarty20@gmail.com>
2018-09-26 15:31:37 -0700

    Added script to gather ping results

---
+++ data_gathering/tcga/ping_gathering.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('/cellar/users/ramarty/Data/kir/validation/PING/run_time.csv', index_col=0)
+barcodes = list(df.Barcode)
+
+
+all_dfs = []
+for barcode in barcodes:
+
+    data = pd.read_csv('/nrnb/users/ramarty/TCGA/exomes/{0}/PING/MIRA_results.csv', index_col=0)
+
+    data[barcode] = data.__KIR
+
+    all_dfs.append(data.loc[:, ['barcode']].transpose())
+
+
+merged = pd.concat(all_dfs)
+
+merged.to_csv('/cellar/users/ramarty/Data/kir/validation/PING/results.csv')
